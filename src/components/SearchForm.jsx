a8bb0b5bc149618f94d0092b56ae929c081c41ac
Scott Schroeder <snschroeder89@gmail.com>
2019-10-14 14:23:45 -0500

    initial commit

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ChordApiService from '../services/chord-api-service';
+
+export default function SearchFrom() {
+  const [key, setKey] = useState('A');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setSearchResults([]);
+    ChordApiService.getChordsByKey(key)
+      .then((chords) => setSearchResults(chords));
+  };
+
+  return (
+    <section>
+      <form className="key-search-form" onSubmit={(ev) => handleSubmit(ev)}>
+        <select onChange={(e) => setKey(e.target.value)}>
+          <option value="A">A Major</option>
+          <option value="B">B Major</option>
+          <option value="C">C Major</option>
+          <option value="D">D Major</option>
+          <option value="E">E Major</option>
+          <option value="F">F Major</option>
+          <option value="G">G Major</option>
+        </select>
+        <button type="submit">Search</button>
+      </form>
+
+      <ul>
+        {searchResults.map((chord) => (
+          <li>{chord.chord_name}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import SearchForm from './components/SearchForm';
+import './index.css';
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<SearchForm />, document.getElementById('root'));
