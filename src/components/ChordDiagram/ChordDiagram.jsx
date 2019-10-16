19c363cd21c3116be6b5350e95c58499a6d66fd9
Scott Schroeder <snschroeder89@gmail.com>
2019-10-16 11:46:55 -0500

    chord diagram generator working. Adding and testing chords

--- src/components/ChordDiagram/ChordDiagram.jsx
+++ src/components/ChordDiagram/ChordDiagram.jsx
@@ -3,22 +3,75 @@ import React, { useState } from 'react';
 import './chord-diagram.css';
 
 export default function ChordDiagram(props) {
-  const c = '0,x,2,1,o,0,o';
+
   const a = '0,x,o,1,1,1,o';
-  const g = '0,2,1,o,o,2,2';
-  const e = '0,o,1,1,0,o,o';
+  const b = '0,x,1,3,3,3,1';
+  const c = '0,x,2,1,o,0,o';
   const d = '0,x,x,o,1,2,1';
+  const e = '0,o,1,1,0,o,o';
   const f = '0,x,2,2,1,0,x';
+  const g = '0,2,1,o,o,2,2';
+
   const am = '0,x,o,1,1,0,o';
+  const bm = '0,x,1,3,3,2,1';
+  const cm = '';
   const dm = '0,x,x,o,1,2,0';
   const em = '0,o,1,1,o,o,o';
+  const fm = '';
+  const gm = '0,2,4,4,2,2,2,b';
+
+  const adim = ''
   const bdim = '0,x,1,2,3,2,x';
-  const b = '0,x,1,3,3,3,1';
-  const fSharpMin = '0,'
+  const cdim = '';
+  const ddim = '';
+  const edim = '0,o,0,1,o,x,x';
+  const fdim = '';
+  const gdim = '';
+
+  const aSh = '';
+  const bSh = '';
+  const cSh = '';
+  const dSh = '';
+  const eSh = '';
+  const fSh = '';
+  const gSh = '';
+
+  const aShM = '';
+  const bShM = '';
+  const cShM = '4,x,0,2,2,1,x';
+  const dShM = '';
+  const eShM = '';
+  const fShM = '0,1,3,3,1,1,1,b';
+  const gShM = '4,0,2,2,0,0,0,b';
+
+  const aB = '';
+  const bB = '0,x,0,2,2,2,0,b';
+  const cB = '';
+  const dB = '';
+  const eB = '';
+  const fB = '';
+  const gB = '';
+
+  const aBM = '';
+  const bBM = '';
+  const cBM = '';
+  const dBM = '';
+  const eBM = '';
+  const fBM = '';
+  const gBM = '';
+
+  const aShDim = '';
+  const bShDim = '';
+  const cShDim = '0,x,1,2,3,2,x';
+  const dShDim = '6,x,0,1,2,1,x';
+
 
   const generateFingerPosition = (position) => {
     let fingerPos = position.split(',');
-    const startFret = fingerPos.shift();
+    const start = fingerPos[0];
+
+    const startFret = start !== '0' ? 'diagram' : 'open-diagram';
+
     fingerPos = fingerPos.map((pos) => {
       if (!'xbo'.includes(pos)) {
         return parseInt(pos, 10);
@@ -28,19 +81,48 @@ export default function ChordDiagram(props) {
     fingerPos = fingerPos.map((pos, index) => {
       let style = {};
       if (typeof (pos) === 'number') {
+        if (index === 0 && pos !== 0) {
+          return (
+            <div className="fret">
+              {pos}
+            </div>
+          );
+        }
+        if (index >= 1) {
+          style = {
+            left: `${((index - 1) * 50) - 15}px`,
+            top: `${(pos * 50) + 13}px`,
+          };
+          return (<div className="finger" style={style} />);
+        }
+      } if (pos === 'x') {
         style = {
-          left: `${(index * 63.6) - 20}px`,
-          top: `${(pos * 63.6) + 13}px`,
+          left: `${(index - 1) * 50 - 13}px`,
+          top: `${-35}px`,
         };
-        return (<div className="finger" style={style} />);
+        return (<div className="muted-string" style={style} />);
+      } if (pos === 'o') {
+        style = {
+          left: `${(index - 1) * 50 - 11}px`,
+          top: `${-34}px`,
+        };
+        return (<div className="open-string" style={style} />);
+      } if (pos === 'b') {
+        return (
+          <svg viewBox="0 0 275 30" className="barre">
+            <path d="M250,30 C250,1 10,1 10,30" />
+          </svg>
+        );
       }
     });
-    return fingerPos;
+    return { startFret, fingerPos };
   };
 
+  const diagramDetails = generateFingerPosition(edim);
+
   return (
-    <div className="diagram">
-      {generateFingerPosition(b)}
+    <div className={diagramDetails.startFret}>
+      {diagramDetails.fingerPos}
     </div>
   );
 }
