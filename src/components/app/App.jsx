7e156d42f0efb84dd9523711618505f24b570a26
Scott Schroeder <snschroeder89@gmail.com>
2019-10-23 11:40:30 -0500

    adjusted folder structure

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import SearchForm from './search-form/SearchForm';
-import AboutPage from './about-page/AboutPage';
-import KeySearchForm from './search-form/KeySearchForm';
-import LandingPage from './landing-page/LandingPage';
-import NotFoundPage from './not-found-page/NotFoundPage';
+import SearchForm from '../search-form/SearchForm';
+import AboutPage from '../about-page/AboutPage';
+import KeySearchForm from '../search-form/KeySearchForm';
+import LandingPage from '../landing-page/LandingPage';
+import NotFoundPage from '../not-found-page/NotFoundPage';
 import './app.css';
 
 function App() {
